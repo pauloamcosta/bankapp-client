dc4f00ad3b8d75160160ee90797c20aeb9846edd
Paulo Costa <pauloamcosta2@gmail.com>
2020-03-09 12:02:47 -0300

    removing isEncrypted logic

--- src/app/components/client-table/client-table.component.ts
+++ src/app/components/client-table/client-table.component.ts
@@ -22,7 +22,6 @@ export class ClientTableComponent implements OnInit {
   }
 
   constructor(
-    private http: HttpClient,
     private apiService: ApiService,
     private snackBar: MatSnackBar) {
   }
@@ -67,10 +66,6 @@ export class ClientTableComponent implements OnInit {
     this.client.id = row.id;
     this.client.name = row.name;
     this.client.account = row.account;
-  }
-
-  isEncrypted(myString: string) {
-    return myString.includes('==');
   }
 
 }
