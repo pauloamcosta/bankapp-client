1bf02e90d92b0a85e94a6157156e8fb428ba213b
Paulo Costa <pauloamcosta2@gmail.com>
2020-03-09 09:13:21 -0300

    fixing issues

--- src/app/components/client-table/client-table.component.ts
+++ src/app/components/client-table/client-table.component.ts
@@ -12,65 +12,69 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./client-table.component.scss']
 })
 export class ClientTableComponent implements OnInit {
+  client = {} as Client;
+  clients: Client[];
+  displayedColumns: string[] = ['id', 'name', 'account'];
+  dataSource;
+
   ngOnInit(): void {
     this.getClients();
   }
 
-  client = {} as Client;
-  clients: Client[];
-
-  dataSource
-
   constructor(
-    private http: HttpClient, 
-    private _apiService: ApiService,
-    private _snackBar: MatSnackBar) {
+    private http: HttpClient,
+    private apiService: ApiService,
+    private snackBar: MatSnackBar) {
   }
+
   openSnackBar(message: string, action: string) {
-    this._snackBar.open(message, action, {
+    this.snackBar.open(message, action, {
       duration: 1500,
     });
   }
+
   getClients() {
-    this._apiService.getAllClients().subscribe((clients: Client[]) => {
+    this.apiService.getAllClients().subscribe((clients: Client[]) => {
       this.clients = clients;
       this.dataSource = new MatTableDataSource(this.clients);
     });
   }
 
   getClient(id: number) {
-    this._apiService.getClientById(id).subscribe((clientDecrypted: Client) => {
+    this.apiService.getClientById(id).subscribe((clientDecrypted: Client) => {
       this.client.name = clientDecrypted.name;
       this.client.account = clientDecrypted.account;
       this.openSnackBar('Client Decrypted', 'x');
     });
   }
 
-  displayedColumns: string[] = ['id', 'name', 'account'];
 
   saveClient(form: NgForm) {
     this.client.id = this.clients.length + 1;
-    this._apiService.saveClient(this.client).subscribe(() => {
-    this.openSnackBar('Client encrypted saved', 'x');
-    this.cleanForm(form);
+    this.apiService.saveClient(this.client).subscribe(() => {
+      this.openSnackBar('Client encrypted saved', 'x');
+      this.cleanForm(form);
     });
 
   }
   cleanForm(form: NgForm) {
-    this.getClients;
     form.resetForm();
     this.client = {} as Client;
     this.ngOnInit();
   }
+
   selectRow(row) {
     this.client.id = row.id;
     this.client.name = row.name;
     this.client.account = row.account;
   }
+
   hasNumber(myString: string) {
     return /\d/.test(myString);
   }
+
   isEncrypted(myString: string) {
     return myString.includes('==');
   }
+
 }
